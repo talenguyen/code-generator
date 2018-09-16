3ab012e25b02183c3477dd90cda0d871b8945213
Tale <giangnguyen.tale@gmail.com>
2018-09-17 04:19:55 +0700

    generate epoxy model from xml

--- epoxy-model/index.js
+++ epoxy-model/index.js
@@ -2,10 +2,30 @@
 
 const fs = require("fs");
 const path = require("path");
-const { parse } = require("./src/package");
+const package = require("./src/package");
+const viewParser = require("./src/view-parser");
+const fileManager = require("./src/file-manager");
+
 const View = require("./src/view");
 const Holder = require("./src/holder");
+const Model = require("./src/model");
 
-const views = [new View("TextView", "text"), new View("ImageView", "image")];
-const holder = new Holder(views);
-console.log(holder.brew());
+const layoutFile = process.argv[2];
+const name = process.argv[3];
+const srcDir = process.argv[4];
+
+//
+const layout = path.basename(layoutFile, ".xml");
+const pkg = package.parse(srcDir);
+const srcFile = `${srcDir}/${name}Model.kt`
+
+fileManager.read(layoutFile)
+  .then(xml => viewParser.parse(xml))
+  .then(views => new Holder(views))
+  .then(holder => new Model(pkg, name, layout, holder))
+  .then(model => fileManager.write(model.brew(), srcFile))
+  .catch(err => console.error(err));
+
+// const views = [new View("TextView", "text"), new View("ImageView", "image")];
+// const holder = new Holder(views);
+// console.log(holder.brew());
